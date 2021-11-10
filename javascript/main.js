7148ba340dce0c4093c8b8d22f89e52d0d270d5b
Enes Baytemir <enesbytmr@gmail.com>
2021-11-10 23:50:32 +0300

    update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,32 +1,77 @@
-//get input
+//Grabbing the input value
 
+var button = document.querySelector("button");
+button.addEventListener("click", function() {
+var input = document.querySelector("input").value;
+getInput(input);
+  
+  
+});
 
-document.querySelector('.js-go').addEventListener('click',()=>{
-    let input = document.querySelector("input").value
-    console.log(input)
-    pushToDom(input);
-    
-})
+var x = document.querySelector(".js-userinput");
+x.addEventListener("keyup", function(e) {
 
-document.querySelector('.js-userinput').addEventListener('keyup',function(e){
-    let input = document.querySelector("input").value
-    if(e.which === 13){
-        pushToDom(input)
-    }
-})
+var input = document.querySelector("input").value;
 
-
-
-console.log(input)
-
-//do the stuff with api
+// 13 is key code for Enter
+if(e.which === 13) {
+getInput(input);
+}
+});
 
 
 
-//showing the giphs
-function pushToDom(input){
-    let container = document.querySelector(".js-container")
-    container.innerHTML = input
+// Interacting with the Giphy API
+
+function getInput(item) {
+  var query = item.split(' ').join('+')
+  
+
+  var url = "https://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=KD9hcp1LItM64RkX6Az7YRRnUwtBgKLv";
+
+
+
+
+// AJAX Request
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open( 'GET', url );
+GiphyAJAXCall.send();
+
+
+GiphyAJAXCall.addEventListener('load', function(e) {
+var data = e.target.response
+pushToDom(data)
+});
+
 }
 
- 
+
+// Showing the gifs
+
+function pushToDom(input) {
+  var response = JSON.parse(input);
+  var f = document.querySelector(".js-container");
+  var result = document.querySelector(".results");
+  
+  clear(f);
+  clear(result);
+    
+      var imageUrls = response.data
+
+      imageUrls.forEach(function(gif) {
+        var src = gif.images.fixed_height.url;
+
+        
+        f.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
+        
+
+      })
+
+     
+      function clear(item) {
+        item.innerHTML = "";
+    }
+
+
+}
+
